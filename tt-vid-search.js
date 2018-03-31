6330cbc1bc66084151be93d84c8912f6e24456c5
Vincent Simone <vsimone@protonmail.com>
2018-03-31 11:13:57 -0700

    Added updateUI function to show displayed and total results

--- tt-vid-search.js
+++ tt-vid-search.js
@@ -4,9 +4,10 @@ function getData(searchTerm, callback) {
   const settings = {
     url: YT_URL,
     data: {
-      part: 'snippet',
-      key: 'AIzaSyAvJA1pHEuF1w6qTrNzvZdT1_-3ua6CYes',
-      q: `${searchTerm}`
+		    'maxResults': 10,
+      	'part': 'snippet',
+      	'key': 'AIzaSyAvJA1pHEuF1w6qTrNzvZdT1_-3ua6CYes',
+      	'q': `${searchTerm}`
     },
     dataType: 'json',
     type: 'GET',
@@ -16,22 +17,34 @@ function getData(searchTerm, callback) {
   $.ajax(settings);
 }
 
+function updateUI(JSON_Object) {
+  let displayedResults = JSON_Object.pageInfo.resultsPerPage;
+  let totalResults =  JSON_Object.pageInfo.totalResults;
+
+  $('.results').append(
+    `<p>Displaying ${displayedResults} of ${totalResults} results.</p>`
+  );
+}
+
 function organizeData(data) {
   console.log('Data organizer running...');
   $('.results').append(`<h2>Here you go:</h2>`);
+	updateUI(data);
   const thumbnailInfo = data.items.map((obj, index) => renderThumbnails(obj));
-  console.log('finished');
+  console.log(data);
   $('.results').append(thumbnailInfo);
 }
 
 function renderThumbnails(JSON_Object) {
   let thumbImg = JSON_Object.snippet.thumbnails.medium.url;
   let thumbLink = `https://www.youtube.com/watch?v=${JSON_Object.id.videoId}`;
+  let vidTitle = JSON_Object.snippet.title
   return `
   <div class="vid">
     <a href="${thumbLink}" target="_blank">
-      <img src="${thumbImg}" alt="YouTube thumbnail" />
+      <img src="${thumbImg}" alt="${vidTitle}" />
     </a>
+    <h2>${vidTitle}</h2>
   </div>`;
 }
 
